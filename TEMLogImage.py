93c1014d765399da69edbffd3730a935d0ca85d6
BraunPenguin <mbraun848@gmail.com>
2020-06-01 18:41:46 -0700

    Implement centered line plotting

--- TEMLogImage.py
+++ TEMLogImage.py
@@ -14,6 +14,29 @@ from matplotlib.artist import Artist
 from skimage.measure import profile_line
 
 
+def offsetToOriginalCoords(coords, centerCoord, returnType='tuple'):
+    assert returnType.lower() == 'numpy' or returnType.lower() == 'tuple', 'Allowed return types are numpy or tuple'
+    if returnType.lower() == 'numpy':
+        return np.asarray([coords[0] + centerCoord[0], coords[1] + centerCoord[1]])
+    return coords[0] + centerCoord[0], coords[1] + centerCoord[1]
+
+
+def originalToOffsetCoords(coords, centerCoord, returnType='tuple'):
+    assert returnType.lower() == 'numpy' or returnType.lower() == 'tuple', 'Allowed return types are numpy or tuple'
+    if returnType.lower() == 'numpy':
+        return np.asarray([coords[0] - centerCoord[0], coords[1] - centerCoord[1]])
+    return coords[0] - centerCoord[0], coords[1] - centerCoord[1]
+
+
+def convertLinePointsToCenteredLinePoints(startPoint, centerCoord):
+    offsetStartPoint = originalToOffsetCoords(startPoint, centerCoord, 'numpy')
+    offsetEndPoint = -offsetStartPoint
+    # if offsetStartPoint[0] > offsetEndPoint[0]:
+    #     offsetStartPoint, offsetEndPoint = offsetEndPoint, offsetStartPoint
+    startPoint = offsetToOriginalCoords(offsetStartPoint, centerCoord)
+    endPoint = offsetToOriginalCoords(offsetEndPoint, centerCoord)
+    return startPoint, endPoint
+
 def dist_point_to_segment(p, s0, s1):
     """
     Get the distance of a point to a segment.
@@ -45,13 +68,16 @@ def dist_point_to_segment(p, s0, s1):
 class PolygonInteractor(object):
     epsilon = 10  # max pixel distance to count as a vertex hit
 
-    def __init__(self, fig, ax, profileax, profileLine, profileLineWidth=1, startPoint=(0, 0), endPoint=(1, 1), pixelScale=1):
+    def __init__(self, fig, ax, profileax, profileLineWidth=1, startPoint=(0, 0), endPoint=(1, 1), pixelScale=1, useCenteredLine=False, centerCoord=(0, 0)):
         self.ax = ax
         self.fig = fig
-        self.profileLine = profileLine
         self.profileax = profileax
         self.pixelScale = pixelScale
-        canvas = ax.figure.canvas
+        self.useCenteredLine = useCenteredLine
+        self.centerCoord = centerCoord
+
+        if self.useCenteredLine:
+            startPoint, endPoint = convertLinePointsToCenteredLinePoints(startPoint, self.centerCoord)
 
         x = [startPoint[0], endPoint[0]]
         y = [startPoint[1], endPoint[1]]
@@ -59,14 +85,14 @@ class PolygonInteractor(object):
         self.line = Line2D(x, y, marker='o', markerfacecolor='r', animated=True)
         self.ax.add_line(self.line)
         self.profileLineWidth = profileLineWidth
+        profileLineData = profile_line(plotData, startPoint, endPoint, linewidth=self.profileLineWidth)
+        self.profileLine = axs[1].plot(self.pixelScale * np.arange(profileLineData.size), profileLineData)[0]
 
         self._ind = None  # the active vertex
-
-        canvas.mpl_connect('draw_event', self.draw_callback)
-        canvas.mpl_connect('button_press_event', self.button_press_callback)
-        canvas.mpl_connect('button_release_event', self.button_release_callback)
-        canvas.mpl_connect('motion_notify_event', self.motion_notify_callback)
-        self.canvas = canvas
+        self.ax.figure.canvas.mpl_connect('draw_event', self.draw_callback)
+        self.ax.figure.canvas.mpl_connect('button_press_event', self.button_press_callback)
+        self.ax.figure.canvas.mpl_connect('button_release_event', self.button_release_callback)
+        self.ax.figure.canvas.mpl_connect('motion_notify_event', self.motion_notify_callback)
 
     # @profile
     def draw_callback(self, event):
@@ -113,35 +139,47 @@ class PolygonInteractor(object):
             return
         x, y = event.xdata, event.ydata
         # print(x, y)
+        print(self._ind)
         self.xy[self._ind] = x, y
+
+        if self.useCenteredLine:
+            _, otherPoint = convertLinePointsToCenteredLinePoints((x, y), self.centerCoord)
+            if self._ind == 0:
+                self.xy[1] = otherPoint
+            else:
+                self.xy[0] = otherPoint
+
         self.line.set_data(zip(*self.xy))
         profileLineData = profile_line(plotData, (self.xy[0][1], self.xy[0][0]), (self.xy[1][1], self.xy[1][0]), linewidth=self.profileLineWidth)
-        self.profileLine.set_xdata(pixelScale * np.arange(profileLineData.size))
+
+        if self.useCenteredLine:
+            xDataLims = (-pixelScale*profileLineData.size/2, pixelScale*profileLineData.size/2)
+            xData = pixelScale * (np.arange(profileLineData.size) - profileLineData.size/2)
+        else:
+            xDataLims = (0, pixelScale*profileLineData.size)
+            xData = pixelScale * np.arange(profileLineData.size)
+        self.profileLine.set_xdata(xData)
         self.profileLine.set_ydata(profileLineData)
-        self.profileax.set_xlim(0, pixelScale*profileLineData.size)
+        self.profileax.set_xlim(xDataLims)
         self.profileax.set_ylim(np.min(profileLineData), np.max(profileLineData))
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
 
-useCenterDistanceCoords = True  # Assumes center is global max
+useCenteredLine = True  # Assumes center is global max
 dmData = dm.dmReader('power removed_ heated to 60C_ SADA 2(1).dm3')
 # dmData = dm.dmReader('16 mW_ follows crystal 2_SADA 2(1).dm3')
 startPoint = (20, 20)
 endPoint = (1000, 2000)
 fig, axs = plt.subplots(figsize=(8, 8), nrows=1, ncols=2)
-nonlogData = dmData['data']+abs(np.min(dmData['data']))+0.000000000000
+nonlogData = dmData['data']+abs(np.min(dmData['data']))
 plotData = np.log10(nonlogData)
-centerX, centerY = np.unravel_index(np.argmax(plotData, axis=None), plotData.shape)
+centerRow, centerCol = np.unravel_index(np.argmax(plotData, axis=None), plotData.shape)
+centerCoord = (centerCol, centerRow)
 axs[0].imshow(plotData, interpolation='none', origin='lower')
 profileLineWidth = 3
-profileLineData = profile_line(plotData, startPoint, endPoint, linewidth=profileLineWidth)
-offsetStartPoint = (startPoint[0]-centerX, startPoint[1]-centerY)
-offsetEndPoint = (endPoint[0]-centerX, endPoint[1]-centerY)
-pixelScale = dmData['pixelSize'][0]  # in 1/nm
-# if offsetStartPoint[0] > offsetEndPoint[0]:
-#     offsetStartPoint, offsetEndPoint = offsetEndPoint, offsetStartPoint
 
-profileLine = axs[1].plot(pixelScale*np.arange(profileLineData.size), profileLineData)[0]
-p = PolygonInteractor(fig, axs[0], axs[1], profileLine, profileLineWidth, startPoint, endPoint, pixelScale)
+pixelScale = dmData['pixelSize'][0]  # in 1/nm
+
+p = PolygonInteractor(fig, axs[0], axs[1], profileLineWidth, startPoint, endPoint, pixelScale, useCenteredLine, centerCoord)
 plt.show()
